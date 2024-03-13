e3f099dd1cb7cb239b1ea298b9db3a8dbcaf8465
haZuny <gkwns5791@naver.com>
2024-03-13 23:03:50 +0900

    동적 웹 페이지 구현

---
+++ 01_SettingView/src/main/java/com/example/hello/MyController.java
@@ -0,0 +1,16 @@
+package com.example.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // MyController 클래스를 컨트롤러로 지정
+public class MyController {
+    @GetMapping("sub-page") // "sub-page"라는 url 주소를 매핑
+    public String subPage(Model model){
+        // "12345678"이라는 값을 "data"라는 이름으로 지정 및 추가
+        model.addAttribute("data", "12345678");
+        // templates 폴터에서 "sub-page.html" 이름을 가지는 템플릿으로 연결
+        return "sub-page";
+    }
+}
