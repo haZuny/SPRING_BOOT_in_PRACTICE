a519495d7dbc95a4175610bbb64fba793ef08a2b
haZuny <gkwns5791@naver.com>
2024-04-26 20:02:07 +0900

    watch collector 프로젝트 생성

---
+++ watch_collector/src/main/java/com/watch_collector/hajun/HajunApplication.java
@@ -0,0 +1,13 @@
+package com.watch_collector.hajun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajunApplication.class, args);
+	}
+
+}
---
+++ watch_collector/src/test/java/com/watch_collector/hajun/HajunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watch_collector.hajun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HajunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
