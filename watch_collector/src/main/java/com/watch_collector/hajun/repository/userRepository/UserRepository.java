7adf93c1e385557f7e4a5b9b454578fa80b68e95
haZuny <gkwns5791@naver.com>
2024-04-27 19:58:40 +0900

    [watchCollector] add UserRepository Interface

---
+++ watch_collector/src/main/java/com/watch_collector/hajun/repository/userRepository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.watch_collector.hajun.repository.userRepository;
+
+import com.watch_collector.hajun.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+    // 사용자 추가
+    User addUser(User user);
+
+    // 사용자 삭제
+    Optional<User> deleteUser(User user);
+
+    // 사용자 id로 찾기
+    Optional<User> findById(long id);
+    
+    // 모든 사용자 리스트 조회
+    List<User> findAll();
+
+
+}
