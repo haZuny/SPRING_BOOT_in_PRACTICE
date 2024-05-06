6c354681fbbbc5c62759ae1102f509829f685f49
haZuny <gkwns5791@naver.com>
2024-05-06 19:33:29 +0900

    after each 추가

--- watch_collector/src/test/java/com/watch_collector/hajun/service/WatchServiceTest.java
+++ watch_collector/src/test/java/com/watch_collector/hajun/service/WatchServiceTest.java
@@ -2,7 +2,9 @@ package com.watch_collector.hajun.service;
 
 import com.watch_collector.hajun.domain.User;
 import com.watch_collector.hajun.domain.Watch;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.util.Assert;
 
 import java.util.ArrayList;
@@ -16,7 +18,13 @@ public class WatchServiceTest {
         return new Watch(userId, "model", 38, "auto", 44, "Mineral", new ArrayList<>());
     }
 
+    @AfterEach
+    public void after_each(){
+        watchService.deleteAllWatches();
+    }
+
     // 시계 추가
+    @Test
     public void 시계추가(){
         // Given
         Watch watch = generateWatch("user1");
@@ -27,6 +35,7 @@ public class WatchServiceTest {
     }
 
     // 시계 찾기
+    @Test
     public void 시계찾기_id(){
         // Given
         Watch watch = generateWatch("user1");
@@ -39,6 +48,7 @@ public class WatchServiceTest {
     }
 
     // 한 사용자의 시계 목록 조회
+    @Test
     public void 시계찾기_User(){
         // Given
         User user1 = new User("user1", "1234");
@@ -57,6 +67,7 @@ public class WatchServiceTest {
     }
 
     // 전체 시계 목록 조회
+    @Test
     public void 시계찾기_All(){
         // Given
         User user1 = new User("user1", "1234");
@@ -73,6 +84,7 @@ public class WatchServiceTest {
     }
 
     // 시계 정보 변경하기
+    @Test
     public void 시계정보변경(){
         // Given
         Watch watch = generateWatch("user1");
@@ -84,6 +96,8 @@ public class WatchServiceTest {
         // Then
         Assertions.assertEquals(changeWatch, changed);
     }
+
+    @Test
     public void 시계정보변경_해당시계없는경우(){
         // Given
         Watch watch = generateWatch("user1");
@@ -97,6 +111,7 @@ public class WatchServiceTest {
     }
 
     // 시계 삭제하기
+    @Test
     public void 시계제거(){
         // Given
         Watch watch = generateWatch("user1");
@@ -108,6 +123,8 @@ public class WatchServiceTest {
         Assertions.assertNull(watchService.findById(watch.getId()));
         Assertions.assertEquals(0, watchService.getAllWatches().size());
     }
+
+    @Test
     public void 시계제거_해당시계없는경우(){
         // Given
         Watch watch = generateWatch("user1");
@@ -120,6 +137,8 @@ public class WatchServiceTest {
         Assertions.assertNotNull(watchService.findById(watch.getId()));
         Assertions.assertEquals(1, watchService.getAllWatches().size());
     }
+
+    @Test
     public void 시계제거_사용자(){
         // Given
         User user1 = new User("user1", "1234");
@@ -136,6 +155,8 @@ public class WatchServiceTest {
         Assertions.assertEquals(2, watchService.getAllWatches().size());
         Assertions.assertEquals(0, watchService.watchesByUser(user1).size());
     }
+
+    @Test
     public void 시계제거_사용자_해당사용자없음(){
         // Given
         User user1 = new User("user1", "1234");
@@ -148,6 +169,8 @@ public class WatchServiceTest {
         // Then
         Assertions.assertEquals(3, watchService.getAllWatches().size());
     }
+
+    @Test
     public void 시계제거_All(){
         // Given
         User user1 = new User("user1", "1234");
