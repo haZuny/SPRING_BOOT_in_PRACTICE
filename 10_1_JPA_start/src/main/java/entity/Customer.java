52e8ccdc6a994fbd6384b31cde7b805085c4adc0
haZuny <gkwns5791@naver.com>
2024-06-07 14:58:17 +0900

    jpa, hibernate 기초 연습

--- 10_1_JPA_start/src/main/java/entity/Customer.java
+++ 10_1_JPA_start/src/main/java/entity/Customer.java
@@ -1,6 +1,14 @@
 package entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// @Entity: jpa를 쓰기 위한 객체임을 명시
+@Entity
+@Table(name="customer_tb")  // 어떤 테이블에 데이터가 들어갈지 명시
 public class Customer {
+    @Id // pk 변수 명시
     private String id;
     private String name;
     private long registerDate;
--- 10_1_JPA_start/src/main/java/jpa/CumtomerJpaExam.java
+++ 10_1_JPA_start/src/main/java/jpa/CumtomerJpaExam.java
@@ -1,4 +1,27 @@
 package jpa;
 
+import entity.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
 public class CumtomerJpaExam {
+    public static void main(String[] args) {
+        // EntityManager를 얻기 위한 Factory 설정
+        // "customer-exam"은 persistence unit name
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("customer-exam");
+
+        // EntityManager
+        EntityManager em = emf.createEntityManager();
+
+        // Transaction을 시작
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        em.persist(Customer.sample());
+        tx.commit();
+
+        em.close();
+        emf.close();
+    }
 }
