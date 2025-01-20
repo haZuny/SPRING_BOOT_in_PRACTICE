852784613c06109f69b7e7f8fb49f91cb388a96f
haZuny <gkwns5791@naver.com>
2025-01-20 19:51:36 +0900

    tdd 플젝 생성

--- servlet/src/main/java/servlet/HelloServlet.java
+++ servlet/src/main/java/servlet/HelloServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,9 +14,19 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        // HTML 파일 경로 설정
+        String htmlPath = "/WEB-INF/hello.html";
+
+        // RequestDispatcher 생성
+        RequestDispatcher dispatcher = request.getRequestDispatcher(htmlPath);
+
         response.setContentType("text/html;charset=UTF-8");
         response.getWriter().println("<html><body>");
         response.getWriter().println("<h1>Hello, Servlet!</h1>");
         response.getWriter().println("</body></html>");
+        // HTML 파일로 포워딩
+//        dispatcher.forward(request, response);
+        dispatcher.include(request, response);
     }
 }
